065c5b6631ba2f2b691de290a8656c767cbc3413
xomimi <sanskritychamling23@gmail.com>
2023-06-13 11:49:46 +0545

    main folder

---
+++ src/main/java/com/example/food_order/FoodOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/food_order/FoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
