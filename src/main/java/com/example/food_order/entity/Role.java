e5f577a728bdc4421289dbb806c8e368fd871241
paribesh-koju <kojuparibesh1234@gmail.com>
2023-06-26 07:35:18 +0545

    role purpose for the user and admin

--- src/main/java/com/example/food_order/entity/Role.java
+++ src/main/java/com/example/food_order/entity/Role.java
@@ -1,2 +1,23 @@
-package com.example.food_order.entity;public class Role {
+package com.example.food_order.entity;
+
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "wpms_role")
+public class Role {
+    @Id
+    @SequenceGenerator(name = "kryss_role_seq_gen",allocationSize = 1,sequenceName = "kryss_role_seq_gen")
+    @GeneratedValue(generator = "kryss_role_seq_gen",strategy = GenerationType.SEQUENCE)
+    private  Integer id;
+
+    @Column(name = "role", nullable = false)
+    private  String name;
 }
