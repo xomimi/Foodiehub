7f0bb476a45bdf97ed24bfd380fe648932e1e25f
paribesh-koju <kojuparibesh1234@gmail.com>
2023-06-29 09:57:38 +0545

    bcrypt the main password

--- src/main/java/com/example/food_order/config/SecurityEncoderUtil.java
+++ src/main/java/com/example/food_order/config/SecurityEncoderUtil.java
@@ -4,4 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SecurityEncoderUtil {
     private static final BCryptPasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+    public static BCryptPasswordEncoder getInstance() {
+        return PASSWORD_ENCODER;
+    }
 }
