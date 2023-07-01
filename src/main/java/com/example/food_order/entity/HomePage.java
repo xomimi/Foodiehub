b5b644b554db8465f62919dd9dd2eb867299695d
paribesh-koju <kojuparibesh1234@gmail.com>
2023-07-01 14:23:24 +0545

    password change and controller

---
+++ src/main/java/com/example/food_order/entity/HomePage.java
@@ -0,0 +1,4 @@
+package com.example.food_order.entity;
+
+public class HomePage {
+}
--- src/main/java/com/example/food_order/repo/UserRepo.java
+++ src/main/java/com/example/food_order/repo/UserRepo.java
@@ -1,4 +1,17 @@
 package com.example.food_order.repo;
 
-public class UserRepo {
+import com.example.food_order.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+    @Query(value = "select * from users where email=?1", nativeQuery = true)
+    Optional<User> findByEmail(String email);
+
+
 }
