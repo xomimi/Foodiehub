9efafb9399fdd7e995ac8796fb92a2ee1ec0ea7d
RiseshStha <riseshstha222@gmail.com>
2023-07-02 14:09:46 +0545

    Adding find by name in item repo

--- src/main/java/com/example/food_order/repo/ItemRepository.java
+++ src/main/java/com/example/food_order/repo/ItemRepository.java
@@ -1,4 +1,10 @@
 package com.example.food_order.repo;
 
-public interface ItemRepository {
+import com.example.food_order.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByNameContainingIgnoreCase(String searchTerm);
 }
