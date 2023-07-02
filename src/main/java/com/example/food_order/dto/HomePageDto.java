d44f5d089827d84e4f1dca7489e9051086f7cda7
RiseshStha <riseshstha222@gmail.com>
2023-07-02 14:45:41 +0545

    Creating ItemDto and Updating Home Page Controller

--- src/main/java/com/example/food_order/dto/HomePageDto.java
+++ src/main/java/com/example/food_order/dto/HomePageDto.java
@@ -1,4 +1,14 @@
 package com.example.food_order.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HomePageDto {
+
 }
