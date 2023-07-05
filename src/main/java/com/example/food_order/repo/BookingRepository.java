c8a25d630cb921f3e2f300af3ab1ef0c2c26ff61
Sonu0103 <sonuhp2002@gmail.com>
2023-07-05 13:11:32 +0545

    add repository page

--- src/main/java/com/example/food_order/repo/BookingRepository.java
+++ src/main/java/com/example/food_order/repo/BookingRepository.java
@@ -1,10 +1,10 @@
-//package com.example.food_order.repo;
-//
-//
-//
-//import com.example.food_order.entity.Booking;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface BookingRepository extends JpaRepository<Booking, Long> {
-//}
-//
+package com.example.food_order.repo;
+
+
+
+import com.example.food_order.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
+
