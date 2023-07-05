9020e4cd90f281de63c2efee7681cf9b163a80b4
Sonu0103 <sonuhp2002@gmail.com>
2023-07-05 13:08:36 +0545

    add booking controller page

--- src/main/java/com/example/food_order/controller/BookingController.java
+++ src/main/java/com/example/food_order/controller/BookingController.java
@@ -1,31 +1,31 @@
-//package com.example.food_order.controller;
-//
-//import com.example.food_order.entity.Booking;
-//import com.example.food_order.repo.BookingRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//@Controller
-//public class BookingController {
-//    private final BookingRepository bookingRepository;
-//
-//    @Autowired
-//    public BookingController(BookingRepository bookingRepository) {
-//        this.bookingRepository = bookingRepository;
-//    }
-//
-//    @GetMapping("/booking-form")
-//    public String showBookingForm(Model model) {
-//        model.addAttribute("booking", new Booking());
-//        return "booking-form";
-//    }
-//
-//    @PostMapping("/booking-form")
-//    public String processBookingForm(Booking booking) {
-//        bookingRepository.save(booking);
-//        return "booking-success";
-//    }
-//}
+package com.example.food_order.controller;
+
+import com.example.food_order.entity.Booking;
+import com.example.food_order.repo.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BookingController {
+    private final BookingRepository bookingRepository;
+
+    @Autowired
+    public BookingController(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    @GetMapping("/booking-form")
+    public String showBookingForm(Model model) {
+        model.addAttribute("booking", new Booking());
+        return "booking-form";
+    }
+
+    @PostMapping("/booking-form")
+    public String processBookingForm(Booking booking) {
+        bookingRepository.save(booking);
+        return "booking-success";
+    }
+}
