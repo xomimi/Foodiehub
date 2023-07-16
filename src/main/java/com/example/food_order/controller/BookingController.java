853562520aaefbc6bd51df5df0426acece246fb9
Sonu0103 <sonuhp2002@gmail.com>
2023-07-16 16:17:29 +0545

    update page

--- src/main/java/com/example/food_order/controller/BookingController.java
+++ src/main/java/com/example/food_order/controller/BookingController.java
@@ -1,14 +1,19 @@
 package com.example.food_order.controller;
 
-import com.example.food_order.entity.Booking;
-import com.example.food_order.repo.BookingRepository;
+// BookingController.java
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.food_order.repo.BookingRepository;
+import com.example.food_order.entity.Booking;
 
 @Controller
+@RequestMapping("/tables")
 public class BookingController {
     private final BookingRepository bookingRepository;
 
@@ -17,15 +22,23 @@ public class BookingController {
         this.bookingRepository = bookingRepository;
     }
 
-    @GetMapping("/booking-form")
-    public String showBookingForm(Model model) {
-        model.addAttribute("booking", new Booking());
-        return "booking-form";
+    @GetMapping("/{id}/book")
+    public String showBookingForm(@ModelAttribute("bookingForm") Booking booking) {
+        return "bookingForm";
     }
 
-    @PostMapping("/booking-form")
-    public String processBookingForm(Booking booking) {
+    @PostMapping("/{id}/book")
+    public String submitBookingForm(@ModelAttribute("bookingForm") Booking booking, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "bookingForm";
+        }
+
+        // Save the booking to the database
         bookingRepository.save(booking);
-        return "booking-success";
+
+        // Add booking details to the model for displaying on the success page
+        model.addAttribute("booking", booking);
+
+        return "bookingSuccess";
     }
 }
