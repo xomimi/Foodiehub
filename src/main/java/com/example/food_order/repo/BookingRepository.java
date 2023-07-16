fe7738162b6b39c68e4627d6543eb595f53a2b8e
Sonu0103 <sonuhp2002@gmail.com>
2023-07-16 16:18:18 +0545

    update page

--- src/main/java/com/example/food_order/repo/BookingRepository.java
+++ src/main/java/com/example/food_order/repo/BookingRepository.java
@@ -1,10 +1,11 @@
 package com.example.food_order.repo;
 
-
-
 import com.example.food_order.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long> {
+@Repository
+
+
+public interface BookingRepository extends jpaRepository<Booking, Long> {
+    void save(Booking booking);
 }
-
