374a032249322d6939b668a830cec025cc0bac0b
Sonu0103 <sonuhp2002@gmail.com>
2023-07-18 20:29:42 +0545

    change in update item page

--- src/main/java/com/example/food_order/config/SecurityEncoderUtil.java
+++
@@ -1,7 +0,0 @@
-package com.example.food_order.config;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class SecurityEncoderUtil {
-    private static final BCryptPasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
-}
---
+++ src/main/java/com/example/food_order/repo/jpaRepository.java
@@ -0,0 +1,4 @@
+package com.example.food_order.repo;
+
+public interface jpaRepository<T, T1> {
+}
