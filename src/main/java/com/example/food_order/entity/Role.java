d8d5bdff96980155fc69033fad10389feef2447d
paribesh-koju <kojuparibesh1234@gmail.com>
2023-08-11 18:06:59 +0545

    role for user and admin

--- src/main/java/com/example/food_order/entity/Role.java
+++ src/main/java/com/example/food_order/entity/Role.java
@@ -1,23 +1,22 @@
-//package com.example.food_order.entity;
-//
-//
-//import jakarta.persistence.*;
-//
-//import lombok.*;
-//
-//@Builder
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Entity
-//@Table(name = "wpms_role")
-//public class Role {
-//    @Id
-//    @SequenceGenerator(name = "kryss_role_seq_gen",allocationSize = 1,sequenceName = "kryss_role_seq_gen")
-//    @GeneratedValue(generator = "kryss_role_seq_gen",strategy = GenerationType.SEQUENCE)
-//    private  Integer id;
-//
-//    @Column(name = "role", nullable = false)
-//    private  String name;
-//}
+package com.example.food_order.entity;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "wpms_role")
+public class Role {
+    @Id
+    @SequenceGenerator(name = "kryss_role_seq_gen",allocationSize = 1,sequenceName = "kryss_role_seq_gen")
+    @GeneratedValue(generator = "kryss_role_seq_gen",strategy = GenerationType.SEQUENCE)
+    private  Integer id;
+
+    @Column(name = "role", nullable = false)
+    private  String name;
+}
