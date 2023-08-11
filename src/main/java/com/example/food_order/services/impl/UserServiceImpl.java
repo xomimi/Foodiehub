d338b9a08f07b62938341db0625e28c4c2cb4640
paribesh-koju <kojuparibesh1234@gmail.com>
2023-08-11 18:19:43 +0545

    Dto replace to pojo user servies to impl

--- src/main/java/com/example/food_order/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/food_order/services/impl/UserServiceImpl.java
@@ -1,50 +1,95 @@
 package com.example.food_order.services.impl;
 
 import com.example.food_order.config.PasswordEncoderUtil;
-import com.example.food_order.dto.UserDto;
 import com.example.food_order.entity.User;
+import com.example.food_order.pojo.UserPojo;
 import com.example.food_order.repo.UserRepo;
 import com.example.food_order.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
     private final UserRepo userRepo;
+
+    private final String UPLOAD_DIRECTORY=System.getProperty("user.dir")+"/photo_file";
     @Override
-    public UserDto save(UserDto userDto) throws IOException {
+    public UserPojo save(UserPojo userPojo) throws IOException {
         User user= new User();
-        user.setId(userDto.getId());
-        user.setEmail(userDto.getEmail());
-        user.setFullName(userDto.getFullname());
-        user.setMobileNo(userDto.getMobile_no());
-        user.setPassword(PasswordEncoderUtil.getInstance().encode(userDto.getPassword()));
+        user.setId(userPojo.getId());
+        user.setEmail(userPojo.getEmail());
+        user.setFullName(userPojo.getFullname());
+        user.setMobileNo(userPojo.getMobile_no());
+        user.setPassword(PasswordEncoderUtil.getInstance().encode(userPojo.getPassword()));
+
+//        if(userPojo.getImage()!=null){
+//            StringBuilder fileNames = new StringBuilder();
+//            System.out.println(UPLOAD_DIRECTORY);
+//            Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, userPojo.getImage().getOriginalFilename());
+//            fileNames.append(userPojo.getImage().getOriginalFilename());
+//            Files.write(fileNameAndPath, userPojo.getImage().getBytes());
+//
+//            user.setImage(userPojo.getImage().getOriginalFilename());
+//        }
 
         user= userRepo.save(user);
-        return new UserDto(user);
+        return new UserPojo(user);
     }
 
     @Override
     public List<User> fetchAll() {
-        return null;
+        return userRepo.findAll();
     }
 
     @Override
-    public User fetchById(Integer id) {
-        return null;
+    public Optional<User> fetchById(Integer id) {
+        return userRepo.findById(id);
     }
 
     @Override
     public void deleteById(Integer id) {
-
+        userRepo.deleteById(id);
     }
 
     @Override
     public void sendEmail() {
 
+    }
+
+    @Override
+    public User fatchByEmail(String email) {
+        return userRepo.findByEmail(email).orElseThrow(() -> new RuntimeException("Not Found"));
+    }
+
+    @Override
+    public void update(UserPojo userPojo) throws IOException {
+        User user= userRepo.findById(userPojo.getId()).get();
+        user.setId(userPojo.getId());
+        user.setEmail(userPojo.getEmail());
+        user.setFullName(userPojo.getFullname());
+        user.setMobileNo(userPojo.getMobile_no());
+        user.setPassword(PasswordEncoderUtil.getInstance().encode(userPojo.getPassword()));
+
+        if(userPojo.getImage()!=null){
+            StringBuilder fileNames = new StringBuilder();
+            System.out.println(UPLOAD_DIRECTORY);
+            Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, userPojo.getImage().getOriginalFilename());
+            fileNames.append(userPojo.getImage().getOriginalFilename());
+            Files.write(fileNameAndPath, userPojo.getImage().getBytes());
+
+            user.setImage(userPojo.getImage().getOriginalFilename());
+        }
+
+        userRepo.save(user);
     }
 }
