260e0675ea26bc8eb032b26999dabc4afbbd9ed9
paribesh-koju <kojuparibesh1234@gmail.com>
2023-08-12 18:33:20 +0545

    html and css of all page display as front end

--- src/main/java/com/example/food_order/security/SpringSecurityConfig.java
+++ src/main/java/com/example/food_order/security/SpringSecurityConfig.java
@@ -1,55 +1,59 @@
-//package com.example.food_order.security;
-//
-//import com.example.food_order.config.PasswordEncoderUtil;
-//import com.example.food_order.services.impl.CustomUserDetailService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SpringSecurityConfig {
-//    private final CustomUserDetailService customUserDetailService;
-//
-//    @Bean
-//    public DaoAuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(customUserDetailService);
-//        authenticationProvider.setPasswordEncoder(PasswordEncoderUtil.getInstance());
-//        return authenticationProvider;
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.csrf().disable()
-//                .authorizeHttpRequests()
-//                .requestMatchers("/login", "/user/**", "/css/**", "/js/**", "/image/**")
-//                .permitAll()
-//                .requestMatchers("*/th-home/**")
-//                .hasAuthority("Admin")
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .defaultSuccessUrl("/th-home/create", true)
-//                .usernameParameter("email")
-//                .permitAll()
-//                .and()
-//                .httpBasic();
-//
-//        return httpSecurity.build();
-//    }
-//}
+package com.example.food_order.security;
+
+import com.example.food_order.config.PasswordEncoderUtil;
+import com.example.food_order.services.impl.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+    private final CustomUserDetailService customUserDetailService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(customUserDetailService);
+        authenticationProvider.setPasswordEncoder(PasswordEncoderUtil.getInstance());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/login","/register","/user/**","/css/**","/js/**","/image/**", "/photo/**","/")
+                .permitAll()
+                .requestMatchers("/th-admin/**")
+                .hasAuthority("admin")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/th-home/create",true)
+                .usernameParameter("email")
+                .permitAll()
+                .and()
+                .httpBasic();
+
+        return httpSecurity.build();
+    }
+
+
+
+
+}
